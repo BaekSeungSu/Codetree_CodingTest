d696078ca9a5b65b4cd048e259fd93b6e380597a
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2026-06-25 23:31:22 +0900

    [Passed] 직각 삼각형 출력 (2ms, 0MB)

---
+++ trail0/직각 삼각형 출력/Right-triangle-and-output.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for(int i=0; i<N; i++)
+    {
+        for(int j = 0; j < 1 + i * 2 ; j++)
+        {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}

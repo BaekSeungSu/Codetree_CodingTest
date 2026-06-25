951329c1fdfbf50f60e455eec74909395be141bf
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2026-06-25 23:59:35 +0900

    [Passed] 숫자 피라미드 2 (2ms, 0MB)

---
+++ trail0/숫자 피라미드 2/number-pyramid-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    int CurrentNum = 1;
+    for(int i=1; i<=N; i++)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            cout << CurrentNum << " ";
+            CurrentNum++;
+        }
+        cout << '\n';
+    }
+    return 0;
+}

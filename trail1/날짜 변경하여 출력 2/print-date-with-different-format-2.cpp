a9c27dc30aea5475cf1882ba2f3c296399bcce30
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2026-06-30 23:10:06 +0900

    [Passed] 날짜 변경하여 출력 2 (2ms, 0MB)

---
+++ trail1/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c;
+    int m, d, y;
+
+    cin >> m >> c >> d >> c >> y;
+    cout << y <<'.' << m << '.' << d;
+    return 0;
+}
